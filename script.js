d271048a9877c590a2ffa426f2b9805afb38ca1e
Gustavo <82328081+GustavoLuisdO@users.noreply.github.com>
2021-10-28 11:05:18 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,27 @@
+function calcular(){
+  var peso = document.getElementById('peso').value
+  var altura = document.getElementById('altura').value
+  var resultado = document.getElementById('resultado')
+
+  if(peso == "" || altura == ""){
+    resultado.innerHTML = `Insira os valores!`
+  }
+
+  var imc = peso/(altura*altura)
+
+  if(imc <= 18.5){
+    resultado.innerHTML = `IMC = ${imc.toFixed(2)} | ABAIXO DO PESO`
+  }
+  else if(imc > 18.5 && imc <= 24.9){
+    resultado.innerHTML = `IMC = ${imc.toFixed(2)} | PESO NORMAL`
+  }
+  else if(imc > 24.9 && imc <= 29.9){
+    resultado.innerHTML = `IMC = ${imc.toFixed(2)} | SOBREPESO`
+  }
+  else if(imc > 29.9 && imc <= 39.9){
+    resultado.innerHTML = `IMC = ${imc.toFixed(2)} | OBESIDADE`
+  }
+  else if(imc > 39.9){
+    resultado.innerHTML = `IMC = ${imc.toFixed(2)} | OBESIDADE GRAVE`
+  }
+}
